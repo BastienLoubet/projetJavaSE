84945d1fa6b7910d0b7729a674e60b0c7e9ee593
BastienLoubet <loubetbastien@yahoo.fr>
2017-03-28 14:12:57 +0200

    Affichage initialisation de la premiere colonne

--- src/affichageEcouteur/Affichage.java
+++ src/affichageEcouteur/Affichage.java
@@ -1,27 +1,59 @@
 package affichageEcouteur;
 
-import java.util.*;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
- * 
+ *  Initialise l'affichage par son constructeur.
  */
 public class Affichage {
 
+    private JFrame oFramePrincipal;
+    private JPanel oPanelMenu;
+    private JButton oBoutonAjouter;
+    private JButton oBoutonRechercher;
+    private JButton oBoutonSauvegarder;
+    private JButton oBoutonCharger;
+
     /**
-     * Default constructor
+     * initialise tout les elements d'affichages
      */
     public Affichage() {
+        initFramePrincipal();
+        initPanelMenu();
+        initBoutonsMenu();
+    }
+    
+    private void initFramePrincipal() {
+        oFramePrincipal = new JFrame("Carnet d'adresses");
+        oFramePrincipal.setVisible(true);
+        oFramePrincipal.setSize(640, 480);
+        oFramePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        oFramePrincipal.setLayout(new GridLayout(1,3));
+    }
+    
+    
+    private void initPanelMenu(){
+        oPanelMenu = new JPanel();
+        oPanelMenu.setLayout(new BoxLayout(oPanelMenu, BoxLayout.Y_AXIS));
+        oFramePrincipal.add(oPanelMenu);
     }
 
-    /**
-     * 
-     */
-    private JFrame oFramePrincipal;
-
-    /**
-     * 
-     */
-    public JPane oFrameMenu;
-
+    private JButton initButtonMenu(String sMes){
+        JButton oButton = new JButton(sMes);
+        oButton.setSize(100, 30);
+        oPanelMenu.add(oButton);
+        return oButton;
+    }
+    
+    private void initBoutonsMenu() {
+        oBoutonAjouter = initButtonMenu("Ajouter");
+        oBoutonRechercher = initButtonMenu("Rechercher");
+        oBoutonCharger = initButtonMenu("Charger");
+        oBoutonSauvegarder = initButtonMenu("Sauvegarder");
+    }
+    
 }
