995a548bdff6f0178286e8b7fe1dd13e59d24cf6
BastienLoubet <loubetbastien@yahoo.fr>
2017-03-28 11:19:45 +0200

    constructeur personne

--- src/personne/Personne.java
+++ src/personne/Personne.java
@@ -7,26 +7,25 @@ import java.util.*;
  */
 public class Personne {
 
-    /**
-     * Default constructor
-     */
-    public Personne() {
-    }
-
-
-
-
-
+    private Nom oNom;
+    private Prenom oPrenom;
+    private Telephone oTelephone;
+    private Adresse oAdresse;
+    private emails oEmails;
 
     /**
-     * @param Nom 
-     * @param Prenom 
-     * @param Telephone 
-     * @param Adresse 
-     * @param Emails
+     * @param sNom Le nom de la personne
+     * @param sPrenom 
+     * @param sTelephone 
+     * @param sAdresse 
+     * @param aEmails Un tableau contenant tous les emails
      */
-    public void Personne(String Nom, String Prenom, String Telephone, String Adresse, ArrayList<String> Emails) {
-        // TODO implement here
+    public void Personne(String sNom, String sPrenom, String sTelephone, String sAdresse, ArrayList<String> aEmails) {
+        oNom = new Nom(sNom);
+        oPrenom = new Prenom(sPrenom);
+        oTelephone = new Telephone(sTelephone);
+        oAdresse = new Adresse(sAdresse);
+        oEmails = new Emails(aEmails);
     }
 
     /**
