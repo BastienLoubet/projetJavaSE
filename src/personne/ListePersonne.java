3b13a6c4a946f384a544bb3c5799091d7ad2383b
BastienLoubet <loubetbastien@yahoo.fr>
2017-03-28 12:15:02 +0200

    ListePersonne

--- src/personne/ListePersonne.java
+++ src/personne/ListePersonne.java
@@ -8,29 +8,54 @@ import java.util.*;
 public class ListePersonne {
 
     /**
-     * Default constructor
+     * 
      */
-    public ListePersonne() {
+    private ArrayList<Personne> aPersonnes;
+    private transient static ListePersonne oInstance;
+
+    
+    /**
+     * Constructeur par defaut initialise aPersonnes
+     */
+    private ListePersonne(){
+        aPersonnes = new ArrayList<Personne>();
+    }
+    
+    
+    /**
+     * 
+     * @return retourne la liste des personnes
+     */
+    public ArrayList<Personne> getPersonnes() {
+        return aPersonnes;
     }
 
     /**
      * 
+     * @param oPersonne l'objet personne a rajouter a la liste
      */
-    public ArrayList<Personne> aPersonnes;
-
-
+    public void add(Personne oPersonne){
+        aPersonnes.add(oPersonne);
+    }
+    
+    
     /**
-     * 
+     * Pour l'implementation du singleton
+     * @return l'instance du singleton
      */
-    public void getPersonnes:ArrayList<Personne>() {
-        // TODO implement here
+    public ListePersonne getInstance() {
+        if(oInstance == null){
+            oInstance = new ListePersonne();
+        }
+        return oInstance;
     }
 
     /**
      * 
+     * @param o la nouvelle instance, pour le chargement
      */
-    public void getInstance:ListePersonne() {
-        // TODO implement here
+    public void setInstance(ListePersonne o){
+        oInstance =o;
     }
-
+    
 }
