d4d9bc829aea9b37b08d464b566771c4f2f24b4a
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-03-29 15:50:59 +0200

    ; dans notre panel

--- src/affichageInfo/NotreJpanelInfo.java
+++ src/affichageInfo/NotreJpanelInfo.java
@@ -36,7 +36,7 @@ public class NotreJpanelInfo extends JPanel implements Observer {
                 this.add(new JLabel("Adresse eMail" +" " + i+ " " + mail));
                 i++;
             }
-        ArrayList<String> aNumTel = oPersonne.GetTelephone()
+        ArrayList<String> aNumTel = oPersonne.GetTelephone();
         for (String num : aNumTel) {
                 this.add(new JLabel("Numéro de téléphone" +" " + i+ " " + num));
                 i++;
