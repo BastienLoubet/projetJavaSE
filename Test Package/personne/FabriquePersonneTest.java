1ba6288c9660aff98150d0b3f6288b1ac9f99969
BastienLoubet <loubetbastien@yahoo.fr>
2017-03-29 10:23:19 +0200

    TestPackgage update

--- Test Package/personne/FabriquePersonneTest.java
+++ Test Package/personne/FabriquePersonneTest.java
@@ -5,11 +5,6 @@
  */
 package personne;
 
-import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,116 +13,24 @@ import static org.junit.Assert.*;
  * @author Garry
  */
 public class FabriquePersonneTest {
-    
-    public FabriquePersonneTest() {
-    }
-    
-
-    /**
-     * Test of getNom method, of class FabriquePersonne.
-     */
-    @Test
-    public void testGetNom() {
-        System.out.println("getNom");
-        FabriquePersonne instance = new FabriquePersonne();
-        instance.getNom();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of String method, of class FabriquePersonne.
-     */
-    @Test
-    public void testString() {
-        System.out.println("String");
-        FabriquePersonne instance = new FabriquePersonne();
-        instance.String();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPrenom method, of class FabriquePersonne.
-     */
-    @Test
-    public void testGetPrenom() {
-        System.out.println("getPrenom");
-        FabriquePersonne instance = new FabriquePersonne();
-        instance.getPrenom();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTelephone method, of class FabriquePersonne.
-     */
-    @Test
-    public void testGetTelephone() {
-        System.out.println("getTelephone");
-        FabriquePersonne instance = new FabriquePersonne();
-        instance.getTelephone();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAdresse method, of class FabriquePersonne.
-     */
-    @Test
-    public void testGetAdresse() {
-        System.out.println("getAdresse");
-        FabriquePersonne instance = new FabriquePersonne();
-        instance.getAdresse();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEmails method, of class FabriquePersonne.
-     */
-    @Test
-    public void testGetEmails() {
-        System.out.println("getEmails");
-        FabriquePersonne instance = new FabriquePersonne();
-        instance.getEmails();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of <error> method, of class FabriquePersonne.
-     */
-    @Test
-    public void test<error>() {
-        System.out.println("<error>");
-        FabriquePersonne instance = new FabriquePersonne();
-        ArrayList<String> expResult = null;
-        ArrayList<String> result = instance.<error>();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ajoutPersonne method, of class FabriquePersonne.
-     */
-    @Test
-    public void testAjoutPersonne() {
-        System.out.println("ajoutPersonne");
-        FabriquePersonne instance = new FabriquePersonne();
-        Boolean expResult = null;
-        Boolean result = instance.ajoutPersonne();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of nomCheck method, of class FabriquePersonne.
      */
     @Test
     public void testNomCheck() {
+        System.out.println("test NomCheck");
+        
+        FabriquePersonne oFabrique = new FabriquePersonne();
+        boolean b;
+        b = oFabrique.nomCheck("Bobby");
+        assertTrue(b);
+        b = oFabrique.nomCheck("121431251");
+        assertFalse(b);
+        b = oFabrique.nomCheck("abcdefghijabcdefghijabcdefghijabcdefghijabcdefghij");
+        assertTrue(b);
+        b = oFabrique.nomCheck("Aabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij");
+        assertFalse(b);
     }
 
     /**
@@ -150,5 +53,5 @@ public class FabriquePersonneTest {
     @Test
     public void testEmailCheck() {
     }
-    
+
 }
